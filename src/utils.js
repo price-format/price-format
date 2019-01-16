3a3e04eca927ca8aa4bb7558a79726c02487df67
Fernando Moreira <nandomoreira.me@gmail.com>
2019-01-16 12:40:26 -0200

    Add Utils is method

--- src/utils.js
+++ src/utils.js
@@ -1,39 +1,97 @@
 /**
- * saySomething
- *
- * import { saySomething } from './utils'
+ * A collection of static utility functions.
+ * @const
  */
+class Utils {
+  /**
+   * deepExtend
+   *
+   * @static
+   */
+  static deepExtend (out) {
+    out = out || {}
 
-export function saySomething (speech = 'Hello 😊') {
-  return speech
-}
+    for (let i = 1; i < arguments.length; i++) {
+      let obj = arguments[i]
 
-/**
- * deepExtend
- *
- * import { deepExtend } from './utils'
- */
+      if (!obj) continue
 
-export const deepExtend = function (out) {
-  out = out || {}
-
-  for (let i = 1; i < arguments.length; i++) {
-    let obj = arguments[i]
-
-    if (!obj) continue
-
-    for (let key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        if (Array.isArray(obj[key])) {
-          out[key] = obj[key]
-        } else if (typeof obj[key] === 'object' && obj[key] !== null) {
-          out[key] = deepExtend(out[key], obj[key])
-        } else {
-          out[key] = obj[key]
+      for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          if (Array.isArray(obj[key])) {
+            out[key] = obj[key]
+          } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+            out[key] = this.deepExtend(out[key], obj[key])
+          } else {
+            out[key] = obj[key]
+          }
         }
       }
     }
+
+    return out
   }
 
-  return out
+  /**
+   * typeOf
+   *
+   * @static
+   * @return string
+   */
+  static typeOf (value) {
+    return this.rawType(value).replace(/^\[object (\w+)]$/, '$1').toLowerCase()
+  }
+
+  /**
+   * isString
+   *
+   * @static
+   * @return boolean
+   */
+  static isString (value) {
+    return typeof value === 'string'
+  }
+
+  /**
+   * hasValue
+   *
+   * @static
+   * @return
+   */
+  static hasValue (params) {
+    return params.length === 2
+  }
+
+  /**
+   * rawType
+   *
+   * @static
+   * @return
+   */
+  static rawType (value) {
+    return Object.prototype.toString.call(value)
+  }
+
+  /**
+   * is
+   *
+   * @static
+   * @return boolean
+   */
+  static is (...params) {
+    const [typeToTest, value] = params
+
+    if (!this.isString(typeToTest)) {
+      throw new TypeError('typeToTest must be a string')
+    }
+
+    if (!this.hasValue(params)) {
+      return (value) => this.is(typeToTest, value)
+    }
+
+    const type = typeToTest.replace(/^\w/, (l) => l.toUpperCase())
+    return this.rawType(value) === `[object ${type}]`
+  }
 }
+
+export default Utils
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,20 +1,8 @@
 import { expect } from 'chai'
-import * as Utils from '../src/utils'
+import Utils from '../src/utils'
 
 describe('Utils', () => {
-  describe('saySomething()', () => {
-    it('Utils.saySomething() be a string', () => {
-      expect(Utils.saySomething()).to.be.a('string')
-    })
-    it('Utils.saySomething() value is equal "Hello 😊"', () => {
-      expect(Utils.saySomething()).to.equal('Hello 😊')
-    })
-    it('Utils.saySomething(\'Hi\') value is equal "Hi"', () => {
-      expect(Utils.saySomething('Hi')).to.equal('Hi')
-    })
-  })
-
-  describe('deepExtend()', () => {
+  describe('Utils.deepExtend()', () => {
     it('Utils.deepExtend() be a object', () => {
       expect(Utils.deepExtend()).to.be.a('object')
     })
@@ -35,6 +23,84 @@ describe('Utils', () => {
     })
     it('Utils.deepExtend([ { a: [1] } ], [ { b: [2] } ]) value is equal [ { a: [ 1 ], b: [ 2 ] } ]', () => {
       expect(Utils.deepExtend([ { a: [1] } ], [ { b: [2] } ])).to.deep.equal([ { a: [ 1 ], b: [ 2 ] } ])
+    })
+  })
+
+  describe('Utils.is()', () => {
+    it('is should be a function', () => {
+      expect(Utils.is).to.be.a('function')
+    })
+
+    it('Utils.is("object", {}) should return true', () => {
+      expect(Utils.is('object', {})).to.be.equal(true)
+    })
+
+    it('Utils.is("object", []) should return false', () => {
+      expect(Utils.is('object', [])).to.be.equal(false)
+    })
+
+    it('Utils.is("object") should return a function (partial application)', () => {
+      expect(Utils.is('object')).to.be.a('function')
+    })
+
+    it('Utils.is("number")(10) should return true', () => {
+      const isNumber = Utils.is('number')
+      expect(isNumber(10)).to.be.equal(true)
+    })
+
+    it('Utils.is(10) should throw an TypeError: "typeToTest must be a string"', () => {
+      try {
+        Utils.is(10)
+      } catch (e) {
+        expect(e.message).to.be.equal('typeToTest must be a string')
+      }
+    })
+
+    it('Utils.is("undefined", undefined) should return true', () => {
+      expect(Utils.is('undefined', undefined)).to.be.equal(true)
+    })
+
+    it('Utils.is("undefined", null) should return false', () => {
+      expect(Utils.is('undefined', null)).to.be.equal(false)
+    })
+
+    it('Utils.is() should throw an TypeError: "typeToTest must be a string"', () => {
+      try {
+        Utils.is()
+      } catch (e) {
+        expect(e.message).to.be.equal('typeToTest must be a string')
+      }
+    })
+  })
+
+  describe('Utils.typeOf()', () => {
+    it('Utils.typeOf should be a function', () => {
+      expect(Utils.typeOf).to.be.a('function')
+    })
+
+    it('Utils.typeOf(123) should return "number"', () => {
+      expect(Utils.typeOf(123)).to.be.equal('number')
+    })
+
+    it('Utils.typeOf({}) should return "object"', () => {
+      expect(Utils.typeOf({})).to.be.equal('object')
+    })
+
+    it('Utils.typeOf([]) should return "array"', () => {
+      expect(Utils.typeOf([])).to.be.equal('array')
+    })
+
+    it('Utils.typeOf(null) should return "null"', () => {
+      expect(Utils.typeOf(null)).to.be.equal('null')
+    })
+
+    it('Utils.typeOf(undefined) should return "undefined"', () => {
+      expect(Utils.typeOf(undefined)).to.be.equal('undefined')
+    })
+
+    it('Utils.typeOf(new String("hey")) should return "string"', () => {
+      // eslint-disable-next-line no-new-wrappers
+      expect(Utils.typeOf(new String('hey'))).to.be.equal('string')
     })
   })
 })
